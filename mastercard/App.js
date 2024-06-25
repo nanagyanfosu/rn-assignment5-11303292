e048793aad30b88dd3acb27e0e63a0ae862a4ae7
nanagyanfosu <nanagyanfosu@gmail.com>
2024-06-25 15:02:49 +0000

    finished header component of page

--- mastercard/App.js
+++ mastercard/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, SafeAreaView } from 'react-native';
+import SearchButton from './SearchButton';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+   <ScrollView>
+<View style={styles.header}>
+<View style={{padding: 0}}>
+<Image source={require('./assets/duo.jpg')} style={{ width: 40, height: 40, borderRadius: 100, margin: 10 }}></Image>
+</View>
+<View style={styles.welcome}>
+<Text style={{fontSize: 21, fontWeight: 200}}>Welcome back, </Text>
+<Text style={{fontSize: 22, fontWeight: 500}}>Nana Gyanfosu</Text>
+</View>
+
+
+<SearchButton/>
+
+</View>
+   </ScrollView>
+   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  header:{
+    padding: 10,
+    flexDirection: 'row',
+    gap: 2,   
+  },
+  
+  welcome:{
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
---
+++ mastercard/SearchButton.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const SearchButton = ({ onPress }) => {
+  return (
+    <View style={{flexDirection: 'row', justifyContent: 'flex-end', width: '45%'  }}>
+    <TouchableOpacity onPress={onPress} style={{alignSelf: 'flex-end'
+    }}>
+        <View style={{padding: 10 ,backgroundColor: 'gray', borderRadius :50, margin:10, }}>
+      <Icon name="magnify" size={24} color="black" />
+      </View>
+    </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchButton;
